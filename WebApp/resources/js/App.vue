a1597aa7cab0ee867824a5fdc1fb938c853b3f15
Joel-B-Johnson <joelbradleyjohnson@gmail.com>
2021-03-25 23:29:30 -0500

    Fortify I want to move back

---
+++ WebApp/resources/js/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <head>
+    </head>
+    <div id="nav">
+        <router-link to="/">Home</router-link>
+    </div>
+    <router-view />
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ WebApp/resources/js/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <head>
+    </head>
+    <h1>Home Page</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
