6872fe60771b822ca88530ab24d274ba5fcf7a45
Joel-B-Johnson <joelbradleyjohnson@gmail.com>
2021-05-04 21:23:02 -0500

    Beautify

--- WebApp/resources/js/pages/Home.vue
+++ WebApp/resources/js/pages/Home.vue
@@ -2,17 +2,28 @@
     <head>
         <title>Rickizon | Home</title>
     </head>
+    <div class="book-container">
+      <BookCard
+        v-for="book in books"
+        :key="book.id"
+        :book="book"
+      />
+    </div>
     <div class="home">
       <img src='../../assets/homeBack.jpg' alt="rick">
     </div>
 </template>
 
 <script>
+import BookCard from "../components/BookCard.vue";
 export default {
   data() {
     return {
       books: "",
     }
+  },
+  components: {
+    BookCard,
   },
   methods: {
     getAllBooks() {
@@ -29,5 +40,11 @@ export default {
 <style lang="scss">
 .home img {
   width: 100%;
+}
+
+.book-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 }
 </style>
