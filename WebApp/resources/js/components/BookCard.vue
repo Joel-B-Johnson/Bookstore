4147b17c8a2b842189772e8814e00be6dc3caa31
Joel-B-Johnson <joelbradleyjohnson@gmail.com>
2021-05-04 21:58:03 -0500

    Books Populating

--- WebApp/resources/js/components/BookCard.vue
+++ WebApp/resources/js/components/BookCard.vue
@@ -1,8 +1,12 @@
 <template>
     <div class="card">
         <h3>{{ book.title }}</h3>
-        <h5>Author: ${{ book.author}}</h5>
+        <h5>Author: {{ book.author }} </h5>
+        <h5>Publisher: {{ book.publisher }} </h5>
+        <h5>ISBN: {{ book.isbn }}</h5>
         <h5 class="price">Price: ${{ book.price.toFixed(2) }}</h5>
+        <h5>Stock: {{ book.stock }}</h5>
+        <button class="view-book-button"></button>
 
  
     </div>
@@ -11,6 +15,9 @@
 <script>
 export default {
     props: ['book'],
+    computed: {
+
+    }
 
 }
 </script>
