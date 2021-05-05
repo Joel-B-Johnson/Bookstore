1b78e7e1df6aaac604b59d6774dfe5e9b97cd69b
Joel-B-Johnson <joelbradleyjohnson@gmail.com>
2021-05-05 12:45:07 -0500

    Some stuff

--- WebApp/resources/js/components/BookCard.vue
+++ WebApp/resources/js/components/BookCard.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="card">
-        <h3>{{ book.title }}</h3>
+        <span>{{ book.title }}</span>
         <h5>Author: {{ book.author }} </h5>
         <h5>Publisher: {{ book.publisher }} </h5>
         <h5>ISBN: {{ book.isbn }}</h5>
         <h5 class="price">Price: ${{ book.price.toFixed(2) }}</h5>
         <h5>Stock: {{ book.stock }}</h5>
-        <button class="view-book-button">View</button>
+        <button class="btn btn-primary btn-blok">View</button>
 
  
     </div>
@@ -23,4 +23,15 @@ export default {
 </script>
 
 <style lang="scss">
+.card span {
+    font-weight: bold;
+    font-size: 1.5em;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    max-width: 30ch;
+}
+.card {
+    padding: 20px 20px 20px 20px;
+}
 </style>
