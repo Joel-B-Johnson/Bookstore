03c511a53a3be8e8eb6b182ac9b146e8756242a9
Joel-B-Johnson <joelbradleyjohnson@gmail.com>
2021-05-04 21:28:41 -0500

    mystuff

--- WebApp/resources/js/components/BookCard.vue
+++ WebApp/resources/js/components/BookCard.vue
@@ -1,13 +1,17 @@
 <template>
-    <div>
- 
+    <div class="card">
+        <h3>{{ book.title }}</h3>
+        <h5>Author: ${{ book.author}}</h5>
+        <h5 class="price">Price: ${{ book.price.toFixed(2) }}</h5>
+
  
     </div>
 </template>
 
 <script>
 export default {
-    
+    props: ['book'],
+
 }
 </script>
 
